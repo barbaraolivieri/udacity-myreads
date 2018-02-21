689db43683ab4d39bf2671608d81a1d6e0a231e6
Barbara Olivieri <barbaranolivieri@gmail.com>
2018-02-21 15:56:20 -0300

    FIX component ListBooks deleting Estante

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,34 +1,45 @@
 import React, {Component} from 'react';
+import Book from './book';
 
+/*
+	ListBooks será o componente responsável pela estrutura
+	básica da estante que determina onde os livros ficarão
+	passando as informações de cada livro de cada categoria.
 
-function ListBooks(props){
-	const { books } = props;
+	A estrutura html referente é o div com a classe 'bookshelf'
+	Não possui estados associados, podendo ser escrita como função
+	Receberá os nomes de cada categorias e livros da API
 
+*/
+
+function ListBooks( {books, updateBooks, categoryTitle} ){
 
 return (
-		<ol className='books-grid'>
+
+    <div className="bookshelf">
+     <h2 className="bookshelf-title">{categoryTitle}</h2>
+      <div className="bookshelf-books">
+        
+		 <ol className='books-grid'>
 			{books.map((book) => (
 				<li key={book.id} > 
-				<div className="book">
-					<div className="book-top">
-						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("{book.imageLinks }")` }} ></div>
-						    <div className="book-shelf-changer">
-                              <select>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option   value="none">None</option>
-                              </select>
-                            </div>
-					</div>
-					<div className="book-title" >{book.title} </div>
-					<div className="book-authors" >{book.authors} </div>
-				</div>
+					<Book title={book.title} authors={book.authors}
+					imageLinks={book.imageLinks} category={book.shelf}
+					updateBooks={updateBooks} id={book.id}
+					 
+					 />
 				</li>
-				) ) }
+			) ) }
 
-		</ol>
+		</ol> 
+
+
+
+      </div>
+    </div>
+
+
+
 
 		)
 
