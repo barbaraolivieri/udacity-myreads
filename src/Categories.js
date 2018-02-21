53eaf980d8f0a2b2936ab33f7d049fa20deb21f6
Barbara Olivieri <barbaranolivieri@gmail.com>
2018-02-21 16:07:13 -0300

    Estrutura básica do  Categories'

--- src/Categories.js
+++ src/Categories.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import ListBooks from './ListBooks';
 
+/*	Esse componente tem como intuito listar as categorias
+	existentes no MyReads, de forma a chamar ListBooks 
+	para preenche-las adequadamente.
+	Como essas categorias referem-se a estados, trata-se
+	de uma classe que fará essa filtragem.
+
+	Tratando do HTML, faremos referencia ao que está incluido
+	no div de classe 'list-books'
+
+*/
+
 class Categories extends Component {
 
+
+	render() {
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+          	<ListBooks/>
+          	<ListBooks/>
+          	<ListBooks/>
+          </div>
+         </div>
+       </div>
+
+
+	}
 
 
 }
