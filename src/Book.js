e9517c982661fa88b45ce724b616251e1a0f0481
Bárbara Olivieri <barbaranolivieri@gmail.com>
2018-02-26 17:14:39 -0300

    FIX none option

--- src/Book.js
+++ src/Book.js
@@ -49,7 +49,7 @@ class Book extends Component {
       			<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div> 
       			    <div className="book-shelf-changer">
                         <select value={ category } onChange={ (event) => this.changeCategory(event.target.value) }>		
-                          <option value="none" disabled>Move to...</option>
+                          <option disabled>Move to...</option>
                           <option value="currentlyReading" >Currently Reading</option>
                           <option value="wantToRead">Want to Read</option>
                           <option value="read">Read</option>
